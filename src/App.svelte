e3d72e49793c457bab1a5fe2d255cd67803e510c
Elbert980 <162024769+Elbert980@users.noreply.github.com>
2024-03-05 18:10:16 +0700

    Creating Cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
-<h1>Hello world</h1>
+<h1>svelte</h1>
+<div class="card-container">
+    <div class="card">
+        <div class="card-image"></div>
+        <div class="card-title">Coding</div>
+        <div class="card-text">
+            spotify, apple music
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-image"></div>
+        <div class="card-title">Cooking</div>
+        <div class="card-text">
+            youtube, google
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-image"></div>
+        <div class="card-title">Cycling</div>
+        <div class="card-text">
+            Bstation, Netflix
+        </div>
+    </div>
+    </div>
